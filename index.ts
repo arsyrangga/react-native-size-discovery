8914ac86e20f3d44b7e684a7f2df6ea526f7e06b
rangga <ranggantwleng77@gmail.com>
2023-07-20 14:11:21 +0700

    react native size discovery

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { Dimensions } from 'react-native';
+
+export let height = Dimensions.get('window').height
+export let width = Dimensions.get('window').width
+
+export const setWidth = (data : number) =>{
+  width = data
+}
+export const setHeight = (data : number) =>{
+  height = data
+}
+// @ts-ignore
+const guidelineBaseWidth = 350;
+export default function scale(size : number) {
+  // @ts-ignore
+  return width / guidelineBaseWidth * size
+};
