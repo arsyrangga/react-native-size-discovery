bdb7a5b9fbed11c84511857ae81f204d0f1bc2df
rangga <ranggantwleng77@gmail.com>
2023-07-20 14:38:33 +0700

    update

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Dimensions } from '../react-native';
+import { Dimensions } from 'react-native';
 
 export let height = Dimensions.get('window').height
 export let width = Dimensions.get('window').width
