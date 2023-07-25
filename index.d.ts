5a50eaaf7912f87a37975f1460969c5c05542fbe
rangga <ranggantwleng77@gmail.com>
2023-07-25 17:38:52 +0700

    update

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module "react-native-size-discovery" {
+    export function scale(size: number): number;
+    export function setWidth(size: number): number;
+    export function setHeight(size: number): number;
+}
--- index.js
+++ index.js
@@ -3,16 +3,19 @@ import { Dimensions } from 'react-native';
 export let height = Dimensions.get('window').height
 export let width = Dimensions.get('window').width
 
-export const setWidth = (data : number) =>{
+export const setWidth = (data) =>{
   width = data
 }
-export const setHeight = (data : number) =>{
+export const setHeight = (data) =>{
   height = data
 }
 // @ts-ignore
 const guidelineBaseWidth = 350;
-export const scale = (size : number) => {
+export const scale = (size) => {
   // @ts-ignore
   const teh = width < height ? width : height
+  if(width > 650){
+  return (teh / 1.75) / guidelineBaseWidth * size
+  }
   return teh / guidelineBaseWidth * size
 };
