07f47b9035b0a01a484a90c90675a0405c5aaf9c
rangga <ranggantwleng77@gmail.com>
2023-07-26 11:35:17 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 export let height = Dimensions.get('window').height
 export let width = Dimensions.get('window').width
+export let isTabletAll = DeviceInfo.isTablet() === true;
+export let isTablet =
+Platform.OS === 'android' && DeviceInfo.isTablet() === true;
+export let isTabletIOS =
+  Platform.OS === 'ios' && DeviceInfo.isTablet() === true;
+
+
 
 export const setWidth = (data) =>{
   width = data
@@ -14,8 +22,8 @@ const guidelineBaseWidth = 350;
 export const scale = (size) => {
   // @ts-ignore
   const teh = width < height ? width : height
-  if(width > 650){
-  return (teh / 1.9) / guidelineBaseWidth * size
+  if(width > 650 && !isTablet && !isTabletAll && isTabletIOS){
+  return (teh / 2) / guidelineBaseWidth * size
   }
   return teh / guidelineBaseWidth * size
 };
